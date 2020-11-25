08cf868a7180e4670726a32ee179b341a7ebcf7d
Tomi Knuuttila <Tomi.Knuuttila@valagroup.com>
2020-11-25 15:47:32 +0200

    Oh my god i know how to use if statement

--- h4444katchu.hs
+++ h4444katchu.hs
@@ -1,2 +1,11 @@
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+  putStrLn "This app answers the h44444katchu question based on input from 1 - 4" 
+  putStrLn "1. Who am I ?" 
+  putStrLn "2. What do I do at Vala" 
+  putStrLn "3. My current customer project" 
+  putStrLn "4. What is my expertise if there is some ?" 
+  c <- getChar
+  if c == '1'
+    then putStrLn "Tomi Knuuttila the it connoisseur"
+  else putChar c
