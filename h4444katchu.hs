0b45e0752da7c54dce1bc7fe6a5f211932c815ed
Tomi Knuuttila <Tomi.Knuuttila@valagroup.com>
2020-11-25 16:15:29 +0200

    Bullshit commit since times up

--- h4444katchu.hs
+++ h4444katchu.hs
@@ -1,11 +1,17 @@
 main :: IO ()
+
 main = do
-  putStrLn "This app answers the h44444katchu question based on input from 1 - 4" 
+  printFirstLines
+
+printFirstLines :: IO ()
+printFirstLines = do 
   putStrLn "1. Who am I ?" 
+  putStrLn "Tomi Knuuttila, companys IT-connoisseur" 
   putStrLn "2. What do I do at Vala" 
+  putStrLn "Software developer, marketing videos and facilitate the cult of DevOps" 
   putStrLn "3. My current customer project" 
+  putStrLn "Aiforia, saving lives each JS line at the time" 
   putStrLn "4. What is my expertise if there is some ?" 
-  c <- getChar
-  if c == '1'
-    then putStrLn "Tomi Knuuttila the it connoisseur"
-  else putChar c
+  putStrLn "Used to be embedded engineer so HW and Ansi C89 are close to my heart" 
+  putStrLn "Been doing some mobile stuff with Xamarin and React Native as well" 
+  putStrLn "And currently doing full-stack stuff with JavaScript, TypeScript, SpedeScript, AWS and what not" 
