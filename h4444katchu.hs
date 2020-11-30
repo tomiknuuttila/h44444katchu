c64da8492be4a5ad8b6d9c80d74d0e7bb1cd5db4
Tomi Knuuttila <Tomi.Knuuttila@valagroup.com>
2020-11-30 20:40:29 +0200

    Update with working IO

--- h4444katchu.hs
+++ h4444katchu.hs
@@ -1,17 +1,28 @@
 main :: IO ()
-
 main = do
   printFirstLines
+  userInput <- getUserInput
+  let answer = getAnswer userInput
+  putStrLn answer
 
 printFirstLines :: IO ()
-printFirstLines = do 
-  putStrLn "1. Who am I ?" 
-  putStrLn "Tomi Knuuttila, companys IT-connoisseur" 
-  putStrLn "2. What do I do at Vala" 
-  putStrLn "Software developer, marketing videos and facilitate the cult of DevOps" 
-  putStrLn "3. My current customer project" 
-  putStrLn "Aiforia, saving lives each JS line at the time" 
-  putStrLn "4. What is my expertise if there is some ?" 
-  putStrLn "Used to be embedded engineer so HW and Ansi C89 are close to my heart" 
-  putStrLn "Been doing some mobile stuff with Xamarin and React Native as well" 
-  putStrLn "And currently doing full-stack stuff with JavaScript, TypeScript, SpedeScript, AWS and what not" 
+printFirstLines = do
+  putStrLn "1. Who am I ?"
+  putStrLn "2. What do I do at Vala"
+  putStrLn "3. My current customer project"
+  putStrLn "4. What is my expertise if there is some ?"
+
+getAnswer :: Char -> String
+getAnswer c 
+  | c == '1' = "Tomi Knuuttila"
+  | c == '2' = "Software developer, marketing videos and facilitate the cult of DevOps"
+  | c == '3' = "Aiforia, saving lives each JS line at the time"
+  | c == '4' = "Pairing natural wines with vegan pizza"
+  | otherwise = "Check your input"
+
+getUserInput :: IO Char
+getUserInput = do
+  c <- getChar
+  if c >= '1' && c <= '4'
+    then return c 
+    else return '0'
